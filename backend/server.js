53c65a79840dc3edf644172d59170c442fa3b9f2
yujin <northwindapps@gmail.com>
2024-12-13 17:15:34 +0900

    donut model route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { PythonShell } = require("python-shell");
+const { spawn } = require("child_process");
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +11,23 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+app.get('/test', async (req, res) => {
+  
+  res.status(200).send("bye");
+});
+
+app.get('/donut', async (req, res) => {
+  PythonShell.run('donut_inference.py', {
+    args: ['./receipt1.png'], // Pass the image file path as an argument
+  }).then(messages => {
+    console.log('Inference Result:', messages);
+    res.status(200).send("ok");
+  }).catch(err => {
+    console.log('Error during inference:', err);
+    res.status(500).send('Error during inference.');
+  });
+});
 
 app.post('/chat', async (req, res) => {
   const { message } = req.body;
